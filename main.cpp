ac9acbf25f3b88ebaec1b1ebddfb7541f5c61694
Brenden-Drumm <158791171+Brenden-Drumm@users.noreply.github.com>
2024-04-15 23:23:59 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,6 +17,21 @@ using namespace std;
 **
 *************************************************************************/
 
+struct Profile
+{
+    string fullname;
+    string state;
+    bool operator==( Profile rhs)
+    {if (fullname==rhs.fullname) return true; return false; }
+    bool operator!=(Profile rhs)
+    {if (fullname!=rhs.fullname) return true; return false; }
+};
+ostream& operator<< (ostream &out, Profile &user)
+{
+    out << user.fullname<<"--" <<user.state;
+    return out;
+}
+
 int main()
 {
     cout << "Hello world!" << endl;
